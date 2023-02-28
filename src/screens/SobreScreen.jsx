980ebefe25d406595ff504a8a78a2647846dc39e
Gabriel Felipe <gabriel.souza16@alunos.sc.senac.br>
2023-02-28 16:37:38 -0300

    up

--- src/screens/SobreScreen.jsx
+++ src/screens/SobreScreen.jsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native"
-import { styles } from "../utils/styles"
+import { useState } from "react";
+import { Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+import { styles } from "../utils/styles";
 
-export const SobreScreen =({navigation}) =>{
-    return(
-        <View  style={styles.container}>
-            <Text style={styles.texto}>Recuperar senha</Text>
-            
-        </View>
-    )
-}
+export const SobreScreen = ({ navigation }) => {
+  const [email, setEmail] = useState({
+    value: "",
+    error: "VOCE AINDA NAO DIGITOU SEU MANE",
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.texto}>Recuperar senha</Text>
+        <Text style={styles.dig}>Digite seu e-mail</Text>
+        <TextInput style={styles.input}
+          label="Digite aqui"
+          value={email.value}
+          onChangeText={(text) => setEmail({ value: text, error: "" })}
+        />
+      </View>
+    </View>
+  );
+};
--- src/utils/styles.jsx
+++ src/utils/styles.jsx
@@ -8,12 +8,29 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#2D4B73",
   },
-  texto: {
+  box: {
     backgroundColor: "#D9D9D9",
-    width: 500,
-    height: 500,
+    width: 373,
+    height: 450,
     borderRadius: 15,
+  },
+  texto: {
     textAlign: "center",
-
+    marginTop: 30,
+    fontSize: 27,
+    fontWeight: "bold",
+  },
+  dig: {
+    marginTop: 60,
+    margin: 30,
+    fontSize: 27,
+  },
+  input: {
+    height: 44,
+    width: 341,
+    margin: 12,
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: "white",
   },
 });
